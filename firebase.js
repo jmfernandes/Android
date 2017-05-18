f82ca4644d5307d34219c4a7d19790371d551b88
jmfernandes <joshfernandes@mac.com>
2017-05-17 17:00:07 -0700

    installed firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/4.0.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAHkWCDexnsrLvi5YBq-3aW-eJE4DRycgw",
+    authDomain: "fir-example-70bc0.firebaseapp.com",
+    databaseURL: "https://fir-example-70bc0.firebaseio.com",
+    projectId: "fir-example-70bc0",
+    storageBucket: "fir-example-70bc0.appspot.com",
+    messagingSenderId: "360218722138"
+  };
+  firebase.initializeApp(config);
+</script>
